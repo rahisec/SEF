6b0b622b7648e256476b4302a92f797c2d52ec28
Kathan Patel <patelkathan22@gmail.com>
2020-12-13 08:55:52 +0530

    Update install.sh
    there was some issue with installation when ran through &>/dev/null so removed it

--- install.sh
+++ install.sh
@@ -19,31 +19,31 @@ logo
 
 #Install Golang.
 echo -e "Installing Go-Lang";
-wget -q -O - https://git.io/vQhTU | bash &>/dev/null
+wget -q -O - https://git.io/vQhTU | bash
 source ~/.bashrc
 echo -e " ";
 
 
 #Install Subfinder.
 echo -e "Installing Subfinder";
-GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder &>/dev/null
+GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
 echo -e " ";
 
 #Install Amass.
 AMASS_VERSION=3.10.5
 echo -e "Installing Amass";
-cd ~ && echo -e "Downloading amass version ${AMASS_VERSION} ..." && wget -q https://github.com/OWASP/Amass/releases/download/v${AMASS_VERSION}/amass_linux_amd64.zip &>/dev/null && unzip amass_linux_amd64.zip &>/dev/null && mv amass_linux_amd64/amass /usr/bin/ &>/dev/null
+cd ~ && echo -e "Downloading amass version ${AMASS_VERSION} ..." && wget -q https://github.com/OWASP/Amass/releases/download/v${AMASS_VERSION}/amass_linux_amd64.zip &>/dev/null && unzip amass_linux_amd64.zip &>/dev/null && mv amass_linux_amd64/amass /usr/bin/
 cd ~ && rm -rf amass_linux_amd64* amass_linux_amd64.zip*
 echo -e " ";
 
 #Install Assetfinder.
 echo -e "Installing Assetfinder";
-go get -u github.com/tomnomnom/assetfinder &>/dev/null
+go get -u github.com/tomnomnom/assetfinder
 echo -e " ";
 
 #Install massdns
 echo "Installing massdns"
-git clone https://github.com/blechschmidt/massdns.git ~/massdns &>/dev/null
+git clone https://github.com/blechschmidt/massdns.git ~/massdns
 cd ~/massdns
 make &>/dev/null
 mv ~/massdns/bin/massdns /usr/local/bin/
@@ -51,7 +51,7 @@ echo -e " ";
 
 #Install Findomain.
 echo -e "Installing Findomain";
-wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux &>/dev/null
+wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux
 sudo chmod +x findomain-linux
 sudo cp findomain-linux /usr/bin/findomain
 sudo rm findomain-linux
@@ -59,32 +59,32 @@ echo -e " ";
 
 #Install Anew.
 echo -e "Installing Anew";
-go get -u github.com/tomnomnom/anew &>/dev/null
+go get -u github.com/tomnomnom/anew
 echo -e " ";
 
 #Install Shuffledns.
 echo -e "Installing shuffledns";
-GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns &>/dev/null
+GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
 echo -e " ";
 
 #Install dnsgen
 echo -e "Installing dnsgen";
-git clone https://github.com/ProjectAnte/dnsgen &>/dev/null
+git clone https://github.com/ProjectAnte/dnsgen
 cd dnsgen
-pip3 install -r requirements.txt &>/dev/null
-sudo python3 setup.py install &>/dev/null
+pip3 install -r requirements.txt
+sudo python3 setup.py install
 cd ..
 echo -e " ";
 
 #Install Httpx
 echo -e "Installing Httpx";
-GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx &>/dev/null
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 echo -e " ";
 
 #Wordlist
 echo -e "Downloading Wordlist";
-curl -O https://s3.amazonaws.com/assetnote-wordlists/data/manual/2m-subdomains.txt &>/dev/null
+curl -O https://s3.amazonaws.com/assetnote-wordlists/data/manual/2m-subdomains.txt
 echo -e "Downloading Resolvers"
-curl -O https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt &>/dev/null
+curl -O https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt
 
 echo -e "Installation Completed.";
